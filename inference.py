870fbd188c631fabaec3a42e4717cb9dd9a2da56
sartq333 <sarthak.jain.min21@itbhu.ac.in>
2026-02-11 22:15:43 +0530

    fix

--- inference.py
+++ inference.py
@@ -2,5 +2,5 @@ from dinov2.models.vision_transformer import vit_small
 
 model = vit_small()
 
-for name, param in model.parameters():
+for name, param in model.named_parameters():
     print("name, param, param.shape: ", name, param, param.shape)
